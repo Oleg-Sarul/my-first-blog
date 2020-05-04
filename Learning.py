3f746b923f4625ba5199a08b2427848dbbba039d
Oleg-Sarul <venturerisb@mail.ru>
2020-05-04 15:39:38 +0300

    My Django First project

---
+++ Learning.py
@@ -0,0 +1,8 @@
+#vokabylary ={'name': 'Oleg' ' Ivan', 'country':'Russia', 'favorit_numbers':[7,42,92]}
+
+def hi(name):
+    print('Hi ' + name)
+mens = ['Oleg', 'Andrew', 'Viktor']
+for name in mens:
+    hi(name)
+    print('Next Men')
